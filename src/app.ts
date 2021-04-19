5edb88c686394c283f9704b8ebbfc93f5307e3c2
Yashraj Mishra <mishra.yashrajs@outlook.com>
2021-04-19 09:42:41 +0530

    repo init

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+// rest of the code remains same
+const app = express();
+const PORT = 3456;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+app.on("exit", () => console.log("Exitted"))
